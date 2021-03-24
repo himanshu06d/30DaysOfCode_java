686abd4525d71f71eebeb915f53986da58224294
Himanshu Dubey <hd156681@gmail.com>
2021-03-24 15:41:01 +0530

    data types

---
+++ Day2.java
@@ -0,0 +1,12 @@
+
+int i2 = scan.nextInt();
+    double d2 = scan.nextDouble();
+    String s2 =scan.next() + scan.nextLine();
+
+    int sum = i + i2;
+    double sumd = d + d2;
+    String con = s.concat(s2);
+
+    System.out.println(sum);
+    System.out.printf("%.1f \n",sumd);
+    System.out.println(con);
